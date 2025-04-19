8c06256f6c1b684db6d2e766266e146477095271
wendingtuo <47069859+wendingtuo@users.noreply.github.com>
2025-04-19 05:06:35 +0200

    Update configuration.nix

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -1,19 +1,17 @@
-{ config, pkgs, stateVersion, hostname, ... }:
+{ pkgs, stateVersion, hostname, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
       ./modules
     ];
   
-  # Networking
   networking.hostName = hostname;
 
-  # Use packages.nix to declare packages
   environment.systemPackages = with pkgs; [
     home-manager
   ];
 
-  system.stateVersion = stateVersion; # Don't touch this!
+  system.stateVersion = stateVersion;
 }
