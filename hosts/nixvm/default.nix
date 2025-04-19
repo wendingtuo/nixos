6a1bc2cfd5cdf2242d744398a877b7c9a1aa55db
blake <bh.nixos@iasus.io>
2025-04-19 10:56:15 +0200

    chasing errors

--- hosts/nixvm/default.nix
+++ hosts/nixvm/default.nix
@@ -9,7 +9,7 @@
     "${nixosModules}/desktop/gnome"
   ];
 
-  networking.hostname = hostname;
+  networking.hostName = hostname;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.11";
--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -120,5 +120,5 @@
 
   # OpenSSH daemon
   services.openssh.enable = true;
-
-};
+  };
+}
