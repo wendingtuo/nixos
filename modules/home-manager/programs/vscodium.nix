93969cd7cce848f63960c96d87c1a9678f04cefb
blake <bh.nixos@iasus.io>
2025-04-26 21:18:18 +0200

    starship and vscodium setup

--- modules/home-manager/programs/vscodium.nix
+++ modules/home-manager/programs/vscodium.nix
@@ -3,22 +3,28 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-    profiles.default.extensions = with pkgs.vscode-extensions; [
-      jnoortheen.nix-ide
-      dracula-theme.theme-dracula
-      redhat.vscode-yaml
-      redhat.ansible
-      ms-azuretools.vscode-docker
-      # ms-kubernetes-tools.vscode-kubernetes-tools
-      # ms-vscode-remote.remote-ssh
-      # hashicorp.terraform
-    # ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-    #   {
-    #     name = "remote-ssh-edit";
-    #     publisher = "ms-vscode-remote";
-    #     version = "0.47.2";
-    #     sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-    #   }
-    ];
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        jnoortheen.nix-ide
+        dracula-theme.theme-dracula
+        redhat.vscode-yaml
+        redhat.ansible
+        ms-azuretools.vscode-docker
+        # ms-kubernetes-tools.vscode-kubernetes-tools
+        # ms-vscode-remote.remote-ssh
+        # hashicorp.terraform
+      # ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      #   {
+      #     name = "remote-ssh-edit";
+      #     publisher = "ms-vscode-remote";
+      #     version = "0.47.2";
+      #     sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+      #   }
+      ];
+      userSettings = {
+        editor.fontFamily = "\"JetBrainsMono Nerd Font\", \"JetBrainsMono Nerd Font\"";
+        workbench.colorTheme = "Dracula Theme";
+      };
+    };
   };
 }
