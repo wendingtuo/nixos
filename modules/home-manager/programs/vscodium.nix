3db51571e4f77f5b6ecb9af3ea15f4c6fac488f3
blake <bh.nixos@iasus.io>
2025-12-08 19:12:47 +0100

    .

--- modules/home-manager/programs/vscodium.nix
+++ modules/home-manager/programs/vscodium.nix
@@ -11,17 +11,6 @@
         tamasfe.even-better-toml
         redhat.vscode-yaml
         redhat.ansible
-        ms-azuretools.vscode-docker
-        # ms-kubernetes-tools.vscode-kubernetes-tools
-        # ms-vscode-remote.remote-ssh
-        # hashicorp.terraform
-      # ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      #   {
-      #     name = "remote-ssh-edit";
-      #     publisher = "ms-vscode-remote";
-      #     version = "0.47.2";
-      #     sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-      #   }
       ];
     };
   };
